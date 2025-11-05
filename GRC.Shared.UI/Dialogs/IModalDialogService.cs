ac1f8c7c8168e261ac67c3ea1889b5b0400700e8
cjordaoc <caio.calisto@br.ey.com>
2025-11-05 15:59:38 -0300

    Centralize GRC Resources

--- GRC.Shared.UI/Dialogs/IModalDialogService.cs
+++
@@ -1,8 +0,0 @@
-using Avalonia.Controls;
-
-namespace GRC.Shared.UI.Dialogs;
-
-public interface IModalDialogService
-{
-    ModalDialogSession Create(Window owner, Control view, string? title = null, ModalDialogOptions? options = null);
-}
